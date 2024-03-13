b265eaf7fb063b50d2e2dad28a538c0480acd529
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2024-03-13 22:34:04 +0100

    Added dark theme

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  /* body.classList.toggle("dark"); */
+
+  if (body.classList.contains("dark-button")) {
+    body.classList.remove("dark-button");
+  } else {
+    body.classList.add("dark-button");
+  }
+}
+
+let themeButton = document.querySelector(".dark-button");
+themeButton.addEventListener("click", changeTheme);
+
 function pressButton() {
   alert("My favorite color is light blue 💙");
 }
 
-let button = document.querySelector("button");
+let button = document.querySelector(".color-button");
 button.addEventListener("click", pressButton);
